88353cd42aa0a9a3f1e9f88c5991bf4b9d2c9b56
Ilya <Termakod@mail.ru>
2022-07-06 17:23:24 +0300

    rrrr

--- Assets/Scripts/LevelChooser.cs
+++ Assets/Scripts/LevelChooser.cs
@@ -6,110 +6,120 @@ using UnityEngine.UI;
 
 public class LevelChooser : MonoBehaviour
 {
-    [SerializeField] private Button ButtonLoad2;
+    [SerializeField] private Button[] ButtonLoad;
+
+    //[SerializeField] private Button ButtonLoad2;
     private int levelIsAvailable2 = 0;
-    [SerializeField] private Button ButtonLoad3;
+    //[SerializeField] private Button ButtonLoad3;
     private int levelIsAvailable3 = 0;
-    [SerializeField] private Button ButtonLoad4;
+    //[SerializeField] private Button ButtonLoad4;
     private int levelIsAvailable4 = 0;
-    [SerializeField] private Button ButtonLoad5;
+    //[SerializeField] private Button ButtonLoad5;
     private int levelIsAvailable5 = 0;
-    [SerializeField] private Button ButtonLoad6;
+    //[SerializeField] private Button ButtonLoad6;
     private int levelIsAvailable6 = 0;
-    [SerializeField] private Button ButtonLoad7;
+    //[SerializeField] private Button ButtonLoad7;
     private int levelIsAvailable7 = 0;
-    [SerializeField] private Button ButtonLoad8;
+    //[SerializeField] private Button ButtonLoad8;
     private int levelIsAvailable8 = 0;
-    [SerializeField] private Button ButtonLoad9;
+    //[SerializeField] private Button ButtonLoad9;
     private int levelIsAvailable9 = 0;
-    [SerializeField] private Button ButtonLoad10;
+    //[SerializeField] private Button ButtonLoad10;
     private int levelIsAvailable10 = 0;
-    [SerializeField] private Button ButtonLoad11;
+    //[SerializeField] private Button ButtonLoad11;
     private int levelIsAvailable11 = 0;
-    [SerializeField] private Button ButtonLoad12;
+    //[SerializeField] private Button ButtonLoad12;
     private int levelIsAvailable12 = 0;
+
+    [SerializeField] private GameObject[] Lockimages;
 
     private int _levelComplete;
 
     private void Start()
     {
-        ButtonLoad2.interactable = false;
-        ButtonLoad3.interactable = false;
-        ButtonLoad4.interactable = false;
-        ButtonLoad5.interactable = false;
-        ButtonLoad6.interactable = false;
-        ButtonLoad7.interactable = false;
-        ButtonLoad8.interactable = false;
-        ButtonLoad9.interactable = false;
-        ButtonLoad10.interactable = false;
-        ButtonLoad11.interactable = false;
-        ButtonLoad12.interactable = false;
+
+        /*ButtonLoad[0].interactable = false;
+        ButtonLoad[1].interactable = false;
+        ButtonLoad[2].interactable = false;
+        ButtonLoad[3].interactable = false;
+        ButtonLoad[4].interactable = false;
+        ButtonLoad[5].interactable = false;
+        ButtonLoad[6].interactable = false;
+        ButtonLoad[7].interactable = false;
+        ButtonLoad[8].interactable = false;
+        ButtonLoad[9].interactable = false;
+        ButtonLoad[10].interactable = false;*/
+
         _levelComplete = PlayerPrefs.GetInt("LevelComplete");
 
-
-        /*for (int i = 1; i < 12; i++)
-        {
-            ButtonLoad2.interactable = true;
-            levelIsAvailable2 = 1;
-            PlayerPrefs.GetInt("LevelIsAvailable2", levelIsAvailable2);
-        }*/
 
         switch (_levelComplete)
         {
             case 1:
-                ButtonLoad2.interactable = true;
+                ButtonLoad[0].interactable = true;
                 levelIsAvailable2 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable2", levelIsAvailable2);
+                Lockimages[0].SetActive(false);
                 break;
             case 2:
-                ButtonLoad3.interactable = true;
+                ButtonLoad[1].interactable = true;
                 levelIsAvailable3 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable3", levelIsAvailable3);
+                Lockimages[1].SetActive(false);
                 break;
             case 3:
-                ButtonLoad4.interactable = true;
+                ButtonLoad[2].interactable = true;
                 levelIsAvailable4 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable4", levelIsAvailable4);
+                Lockimages[2].SetActive(false);
                 break;
             case 4:
-                ButtonLoad5.interactable = true;
+                ButtonLoad[3].interactable = true;
                 levelIsAvailable5 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable5", levelIsAvailable5);
+                Lockimages[3].SetActive(false);
                 break;
             case 5:
-                ButtonLoad6.interactable = true;
+                ButtonLoad[4].interactable = true;
                 levelIsAvailable6 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable6", levelIsAvailable6);
+                Lockimages[4].SetActive(false);
                 break;
             case 6:
-                ButtonLoad7.interactable = true;
+                ButtonLoad[5].interactable = true;
                 levelIsAvailable7 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable7", levelIsAvailable7);
+                Lockimages[5].SetActive(false);
                 break;
             case 7:
-                ButtonLoad8.interactable = true;
+                ButtonLoad[6].interactable = true;
                 levelIsAvailable8 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable8", levelIsAvailable8);
+                Lockimages[6].SetActive(false);
                 break;
             case 8:
-                ButtonLoad9.interactable = true;
+                ButtonLoad[7].interactable = true;
                 levelIsAvailable9 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable9", levelIsAvailable9);
+                Lockimages[7].SetActive(false);
                 break;
             case 9:
-                ButtonLoad10.interactable = true;
+                ButtonLoad[8].interactable = true;
                 levelIsAvailable10 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable10", levelIsAvailable10);
+                Lockimages[8].SetActive(false);
                 break;
             case 10:
-                ButtonLoad11.interactable = true;
+                ButtonLoad[9].interactable = true;
                 levelIsAvailable11 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable11", levelIsAvailable11);
+                Lockimages[9].SetActive(false);
                 break;
             case 11:
-                ButtonLoad12.interactable = true;
+                ButtonLoad[11].interactable = true;
                 levelIsAvailable12 = 1;
                 PlayerPrefs.GetInt("LevelIsAvailable12", levelIsAvailable12);
+                Lockimages[10].SetActive(false);
                 break;
             default:
                 Debug.Log("Не прошел еще");
@@ -119,81 +129,116 @@ public class LevelChooser : MonoBehaviour
         levelIsAvailable2 = PlayerPrefs.GetInt("LevelIsAvailable2");
         if (levelIsAvailable2 == 1)
         {
-            ButtonLoad2.interactable = true;
+            ButtonLoad[0].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[0].interactable = false;
         }
 
         levelIsAvailable3 = PlayerPrefs.GetInt("LevelIsAvailable3");
         if (levelIsAvailable3 == 1)
         {
-            ButtonLoad3.interactable = true;
+            ButtonLoad[1].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[1].interactable = false;
         }
 
         levelIsAvailable4 = PlayerPrefs.GetInt("LevelIsAvailable4");
         if (levelIsAvailable4 == 1)
         {
-            ButtonLoad4.interactable = true;
+            ButtonLoad[2].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[2].interactable = false;
         }
 
         levelIsAvailable5 = PlayerPrefs.GetInt("LevelIsAvailable5");
         if (levelIsAvailable5 == 1)
         {
-            ButtonLoad5.interactable = true;
+            ButtonLoad[3].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[3].interactable = false;
         }
 
         levelIsAvailable6 = PlayerPrefs.GetInt("LevelIsAvailable6");
         if (levelIsAvailable6 == 1)
         {
-            ButtonLoad6.interactable = true;
+            ButtonLoad[4].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[4].interactable = false;
         }
 
         levelIsAvailable7 = PlayerPrefs.GetInt("LevelIsAvailable7");
         if (levelIsAvailable7 == 1)
         {
-            ButtonLoad7.interactable = true;
+            ButtonLoad[5].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[5].interactable = false;
         }
 
         levelIsAvailable8 = PlayerPrefs.GetInt("LevelIsAvailable8");
         if (levelIsAvailable8 == 1)
         {
-            ButtonLoad8.interactable = true;
+            ButtonLoad[6].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[6].interactable = false;
         }
 
         levelIsAvailable9 = PlayerPrefs.GetInt("LevelIsAvailable9");
         if (levelIsAvailable9 == 1)
         {
-            ButtonLoad9.interactable = true;
+            ButtonLoad[7].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[7].interactable = false;
         }
 
         levelIsAvailable10 = PlayerPrefs.GetInt("LevelIsAvailable10");
         if (levelIsAvailable10 == 1)
         {
-            ButtonLoad10.interactable = true;
+            ButtonLoad[8].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[8].interactable = false;
         }
 
         levelIsAvailable11 = PlayerPrefs.GetInt("LevelIsAvailable11");
         if (levelIsAvailable11 == 1)
         {
-            ButtonLoad11.interactable = true;
+            ButtonLoad[9].interactable = true;
+        }
+        else
+        {
+            ButtonLoad[9].interactable = false;
         }
 
         levelIsAvailable12 = PlayerPrefs.GetInt("LevelIsAvailable12");
         if (levelIsAvailable12 == 1)
         {
-            ButtonLoad12.interactable = true;
+            ButtonLoad[10].interactable = true;
         }
-
-        /*if (_levelComplete == 1)
+        else
         {
-            ButtonLoad2.interactable = true;
-            levelIsAvailable2 = 1;
-            PlayerPrefs.SetInt("LevelIsAvilable2", levelIsAvailable2);
-        }*/
+            ButtonLoad[10].interactable = false;
+        }
     }
     public void LoadLevel(int Level)
     {
         SceneManager.LoadScene(Level);
     }
-
-
 
 }
--- Assets/Scripts/StuffManager.cs
+++ Assets/Scripts/StuffManager.cs
@@ -9,6 +9,7 @@ public class StuffManager : MonoBehaviour
     public float _money = 0;
     public int _carsWereParked = 0;
     [SerializeField] Text carScore;
+    [SerializeField] Text clock;
 
     private int LevelComplete = 0;
 
@@ -30,22 +31,45 @@ public class StuffManager : MonoBehaviour
 
     public LevelNumber levelNumber;
 
-    private List<int> carToChangeLevel = new List<int>()
+    private List<int> timeToChangeLevel = new List<int>()
     {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
     };
+
+    DateTime date1 = new DateTime();
+
     private void Start()
     {
         CarsMoveParking.wasParked += AddCarScore;
+        date1 = date1.AddHours(9);
+        clock.text = date1.ToString("HH:mm");
     }
-
     
+    private void FixedUpdate()
+    {
+        date1 = date1.AddSeconds(1);
+        clock.text = date1.ToString("HH:mm");
+        for (int i = 1; i < timeToChangeLevel.Count; i++)
+        {
+            
+                if (date1.Hour == 18)
+                {
+                    LevelComplete = i;
+                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
+                    SceneManager.LoadScene($"Level_{i + 1}");
+                }
+            
+        }
+    }
 
     void AddCarScore()
     {
         _carsWereParked++;
+        carScore.text = _carsWereParked.ToString();
+        Debug.Log(date1.Hour);
+        
 
-        for (int i = 1; i < carToChangeLevel.Count; i++)
+        /*for (int i = 1; i < carToChangeLevel.Count; i++)
         {
             if ((int)levelNumber == i)
             {
@@ -58,111 +82,7 @@ public class StuffManager : MonoBehaviour
                 }
             }
 
-        }
-        /*switch (ff)
-        {
-            case 1:
-                if (_carsWereParked == 1)
-                {
-                    LevelComplete = 1;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_2");
-                }
-                break;
-            case 2:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_3");
-                }
-                break;
-            case 3:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_4");
-                }
-                break;
-            case 4:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_5");
-                }
-                break;
-            case 5:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_6");
-                }
-                break;
-            case 6:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_7");
-                }
-                break;
-            case 7:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_8");
-                }
-                break;
-            case 8:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_9");
-                }
-                break;
-            case 9:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_10");
-                }
-                break;
-            case 10:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_11");
-                }
-                break;
-            case 11:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Level_12");
-                }
-                break;
-            case 12:
-                if (_carsWereParked == 2)
-                {
-                    LevelComplete = 2;
-                    PlayerPrefs.SetInt("LevelComplete", LevelComplete);
-                    SceneManager.LoadScene("Menu");
-                }
-                break;
-            default:
-                Debug.Log("ggg");
-                break;
         }*/
-
-
     }
 }
     
